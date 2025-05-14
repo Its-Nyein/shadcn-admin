60f2adbc00eb930cb65ccb46d0d6425d83b552c8
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-05-15 01:11:17 +0630

    feat(dashboard): add not found page and dashboard top navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
+import { DashboardTopNav } from "@/components/dashboard-top-nav";
 import { Header } from "@/components/header";
 import { ProfileDropdown } from "@/components/profile-dropdown";
 import { Search } from "@/components/search";
 import { ToggleTheme } from "@/components/toggle-theme";
 
+const topNav = [
+  {
+    title: 'Overview',
+    href: 'dashboard/overview',
+    isActive: true,
+    disabled: false,
+  },
+  {
+    title: 'Customers',
+    href: 'dashboard/customers',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Products',
+    href: 'dashboard/products',
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: 'Settings',
+    href: 'dashboard/settings',
+    isActive: false,
+    disabled: true,
+  },
+]
+
 export default function DashboardPage() {
   return (
     <div className="w-full">
       <Header>
-        <Search />
-        <div className='ml-auto flex items-center space-x-4'>
+        <DashboardTopNav links={topNav}/>
+        <div className="ml-auto flex items-center space-x-4">
+          <Search />
           <ToggleTheme />
           <ProfileDropdown />
         </div>
---
+++ components/dashboard-top-nav.tsx
@@ -0,0 +1,75 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import React from "react";
+import { Button } from "./ui/button";
+import { IconMenu } from "@tabler/icons-react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+interface DashboardTopNavProps extends React.HTMLAttributes<HTMLElement> {
+  links: {
+    title: string;
+    href: string;
+    isActive: boolean;
+    disabled?: boolean;
+  }[];
+}
+
+export function DashboardTopNav({
+  className,
+  links,
+  ...props
+}: DashboardTopNavProps) {
+  return (
+    <>
+      <div className="md:hidden">
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon" variant="outline">
+              <IconMenu />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            {links.map(({ title, href, isActive, disabled }) => (
+              <DropdownMenuItem key={`${title}-${href}`}>
+                <Link
+                  href={href}
+                  className={isActive ? "text-muted-foreground" : ""}
+                  aria-disabled={disabled}
+                >
+                  {title}
+                </Link>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <nav
+        className={cn(
+          "hidden items-center space-x-4 md:flex lg:space-x-6",
+          className
+        )}
+        {...props}
+      >
+        {links.map(({ title, href, isActive, disabled }) => (
+          <Link
+            key={`${title}-${href}`}
+            href={href}
+            aria-disabled={disabled}
+            className={`hover:text-primary text-sm font-medium transition-colors ${
+              isActive ? "" : "text-muted-foreground"
+            }`}
+          >
+            {title}
+          </Link>
+        ))}
+      </nav>
+    </>
+  );
+}
