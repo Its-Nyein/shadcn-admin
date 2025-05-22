80685d307d58b55d63c438a11c1b519cd16b0cd5
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-05-23 00:02:44 +0630

    chore(dashboard): add dashboard tabs and contents cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,83 @@
 import { DashboardTopNav } from "@/components/dashboard-top-nav";
 import { Header } from "@/components/header";
+import { Main } from "@/components/main";
 import { ProfileDropdown } from "@/components/profile-dropdown";
 import { Search } from "@/components/search";
 import { ToggleTheme } from "@/components/toggle-theme";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { DashboardCard } from "@/features/components/dashboard-card";
 
 const topNav = [
   {
-    title: 'Overview',
-    href: 'dashboard/overview',
+    title: "Overview",
+    href: "dashboard/overview",
     isActive: true,
     disabled: false,
   },
   {
-    title: 'Customers',
-    href: 'dashboard/customers',
+    title: "Customers",
+    href: "dashboard/customers",
     isActive: false,
     disabled: true,
   },
   {
-    title: 'Products',
-    href: 'dashboard/products',
+    title: "Products",
+    href: "dashboard/products",
     isActive: false,
     disabled: true,
   },
   {
-    title: 'Settings',
-    href: 'dashboard/settings',
+    title: "Settings",
+    href: "dashboard/settings",
     isActive: false,
     disabled: true,
   },
-]
+];
 
 export default function DashboardPage() {
   return (
     <div className="w-full">
       <Header>
-        <DashboardTopNav links={topNav}/>
+        <DashboardTopNav links={topNav} />
         <div className="ml-auto flex items-center space-x-4">
           <Search />
           <ToggleTheme />
           <ProfileDropdown />
         </div>
       </Header>
-      <div className="p-4 sm:gap-4">Hello DashboardPage</div>
+      <Main>
+        <div className="mb-2 flex items-center justify-between space-y-2">
+          <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
+          <div className="flex items-center space-x-2">
+            <Button>Download</Button>
+          </div>
+        </div>
+        <Tabs
+          orientation="vertical"
+          defaultValue="overview"
+          className="space-y-4"
+        >
+          <div className="w-full overflow-x-auto pb-2">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics" disabled>
+                Analytics
+              </TabsTrigger>
+              <TabsTrigger value="reports" disabled>
+                Reports
+              </TabsTrigger>
+              <TabsTrigger value="notifications" disabled>
+                Notifications
+              </TabsTrigger>
+            </TabsList>
+          </div>
+          
+          <TabsContent value='overview' className='space-y-4'>
+            <DashboardCard/>
+          </TabsContent>
+        </Tabs>
+      </Main>
     </div>
   );
 }
