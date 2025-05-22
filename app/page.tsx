51bac607c07b56be9388330ecd4db8b0d2c2be86
nyeinphyoaung <nyeinphyoaung.edu@gmail.com>
2025-05-23 00:36:29 +0630

    feat(dashboard): add dashboard overview and recent sales cards

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import { ProfileDropdown } from "@/components/profile-dropdown";
 import { Search } from "@/components/search";
 import { ToggleTheme } from "@/components/toggle-theme";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { DashboardCard } from "@/features/components/dashboard-card";
+import { DashboardOverview } from "@/features/components/dashboard-overview";
+import { DashboardRecentSales } from "@/features/components/dashboard-recent-sales";
 
 const topNav = [
   {
@@ -72,9 +75,31 @@ export default function DashboardPage() {
               </TabsTrigger>
             </TabsList>
           </div>
-          
-          <TabsContent value='overview' className='space-y-4'>
-            <DashboardCard/>
+
+          <TabsContent value="overview" className="space-y-4">
+            <DashboardCard />
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-7">
+              <Card className="col-span-1 lg:col-span-4">
+                <CardHeader>
+                  <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className="pl-2">
+                  <DashboardOverview />
+                </CardContent>
+              </Card>
+
+              <Card className='col-span-1 lg:col-span-3'>
+                <CardHeader>
+                  <CardTitle>Recent Sales</CardTitle>
+                  <CardDescription>
+                    You made 265 sales this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <DashboardRecentSales/>
+                </CardContent>
+              </Card>
+            </div>
           </TabsContent>
         </Tabs>
       </Main>
---
+++ features/components/dashboard-recent-sales.tsx
@@ -0,0 +1,85 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+export function DashboardRecentSales() {
+  return (
+    <div className='space-y-8'>
+      <div className='flex items-center gap-4'>
+        <Avatar className='h-9 w-9'>
+          <AvatarImage src='/avatars/06.png' alt='Avatar' />
+          <AvatarFallback>AR</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-1 flex-wrap items-center justify-between'>
+          <div className='space-y-1'>
+            <p className='text-sm leading-none font-medium'>Ava Rodriguez</p>
+            <p className='text-muted-foreground text-sm'>
+              ava.rodriguez@email.com
+            </p>
+          </div>
+          <div className='font-medium'>+$2,450.00</div>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-4'>
+        <Avatar className='h-9 w-9'>
+          <AvatarImage src='/avatars/07.png' alt='Avatar' />
+          <AvatarFallback>LM</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-1 flex-wrap items-center justify-between'>
+          <div className='space-y-1'>
+            <p className='text-sm leading-none font-medium'>Liam Murphy</p>
+            <p className='text-muted-foreground text-sm'>
+              liam.murphy@email.com
+            </p>
+          </div>
+          <div className='font-medium'>+$135.00</div>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-4'>
+        <Avatar className='h-9 w-9'>
+          <AvatarImage src='/avatars/08.png' alt='Avatar' />
+          <AvatarFallback>CH</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-1 flex-wrap items-center justify-between'>
+          <div className='space-y-1'>
+            <p className='text-sm leading-none font-medium'>Chloe Harris</p>
+            <p className='text-muted-foreground text-sm'>
+              chloe.harris@email.com
+            </p>
+          </div>
+          <div className='font-medium'>+$720.00</div>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-4'>
+        <Avatar className='h-9 w-9'>
+          <AvatarImage src='/avatars/09.png' alt='Avatar' />
+          <AvatarFallback>EM</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-1 flex-wrap items-center justify-between'>
+          <div className='space-y-1'>
+            <p className='text-sm leading-none font-medium'>Ethan Moore</p>
+            <p className='text-muted-foreground text-sm'>ethan.moore@email.com</p>
+          </div>
+          <div className='font-medium'>+$560.00</div>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-4'>
+        <Avatar className='h-9 w-9'>
+          <AvatarImage src='/avatars/10.png' alt='Avatar' />
+          <AvatarFallback>GR</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-1 flex-wrap items-center justify-between'>
+          <div className='space-y-1'>
+            <p className='text-sm leading-none font-medium'>Grace Reynolds</p>
+            <p className='text-muted-foreground text-sm'>
+              grace.reynolds@email.com
+            </p>
+          </div>
+          <div className='font-medium'>+$85.00</div>
+        </div>
+      </div>
+    </div>
+  )
+}
