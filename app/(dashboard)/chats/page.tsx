bde1b58e3cd504c30decc767c2b36a94f64a09fc
nyein phyo aung <146245393+Its-Nyein@users.noreply.github.com>
2025-11-24 16:37:56 +0630

    feat: added beautiful chat page

--- app/(dashboard)/chats/page.tsx
+++ app/(dashboard)/chats/page.tsx
@@ -1,5 +1,19 @@
-import { ComingSoon } from "@/components/coming-soon";
+import chatConversations from "@/constants/chat-conversation.json";
+import chatMessages from "@/constants/chat-messages.json";
+import chatUsers from "@/constants/chat-users.json";
+import { Chat } from "@/features/chats/components/chats";
+import {
+  ChatConversation,
+  ChatMessage,
+  ChatUser,
+} from "@/features/chats/utils/types";
 
 export default function ChatsPage() {
-  return <ComingSoon />;
+  return (
+    <Chat
+      conversations={chatConversations as ChatConversation[]}
+      messages={chatMessages as Record<string, ChatMessage[]>}
+      users={chatUsers as ChatUser[]}
+    />
+  );
 }
