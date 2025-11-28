769b6ffc51874ec3a94bc777e28916789b06465c
nyein phyo aung <146245393+Its-Nyein@users.noreply.github.com>
2025-11-28 21:54:19 +0630

    chore: fixed build issues in vercel

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,6 +6,8 @@ import { isAuthenticated } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
   children,
 }: {
