7207fa46234ac619ebdb6c729a2337a0d2be1a28
nyein phyo aung <146245393+Its-Nyein@users.noreply.github.com>
2026-02-07 14:39:50 +0630

    chore: fixed better auth invalid url issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL!,
+  trustedOrigins: [process.env.BETTER_AUTH_URL!],
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
